8b382d91faf8cd1a90dc2bdd3250a5b8598cd95b
seungin <seungin.cha@gmail.com>
2020-08-01 22:58:53 +0900

    Make a simple project
     - Create a CMake project
     - Write a build guide

---
+++ CMake/CompileOptions.cmake
@@ -0,0 +1,25 @@
+# 
+# Platform and architecture setup
+# 
+
+# Project options
+set(DEFAULT_PROJECT_OPTIONS
+  CXX_STANDARD 17
+  LINKER_LANGUAGE CXX
+  POSITION_INDEPENDENT_CODE ON
+)
+
+# Include directories
+set(DEFAULT_INCLUDE_DIRECTORIES)
+
+# Libraries
+set(DEFAULT_LIBRARIES)
+
+# Compile definitions
+set(DEFAULT_COMPILE_DEFINITIONS)
+
+# Compile options
+set(DEFAULT_COMPILE_OPTIONS)
+
+# Linker options
+set(DEFAULT_LINKER_OPTIONS)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cpp-oss-template
+
 C++ Open Source Software Template
+
+### build
+
+```sh
+$ mkdir build && cd build
+$ cmake ..
+$ cmake --build .
+$ ls -l lib
+-rw-rw-r-- 1 seungin seungin 1498 Aug  1 22:49 libcpp-oss-template.a
+```
